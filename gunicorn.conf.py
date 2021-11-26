74cc56fea8e3ddcb10ec800aeabb850c7efa93b5
Biswajee <roy.biswajeet161@gmail.com>
2021-11-26 09:34:05 +0530

    Update address

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ from os import environ
 
 port = environ.get('PORT')
 if port is None:
-    bind = "127.0.0.1:8080"
+    bind = "0.0.0.0:8080"
 else:
-    bind = f"127.0.0.1:{port}"
+    bind = f"0.0.0.0:{port}"
 workers = multiprocessing.cpu_count() * 2 + 1
