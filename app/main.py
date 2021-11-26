f62f753077448dd4e417cf9034c91734a510df47
Biswajee <roy.biswajeet161@gmail.com>
2021-11-26 19:40:51 +0530

    Add more security

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+import re
 import json
 import subprocess
 from typing import Any, List
 
 from flask import Flask, jsonify, request
+from json.decoder import JSONDecodeError
 
 # Instantiating Flask object
 app = Flask(__name__)
@@ -16,19 +18,27 @@ def index():
 
 @app.route("/certigo")
 def certigo_util():
-    send = {
-        "status": "OK",
-        "certificates": "a domain was not provided",
-    }
-    domain = request.args["domain"]
-    if domain is not None:
-        result = parse_certificate(domain)
-    if result != "fail":
+    try:
         send = {
             "status": "OK",
-            "certificates": result,
+            "certificates": "a domain was not provided",
         }
-    return jsonify(send)
+        domain = request.args["domain"]
+        result = sanitize_input(domain)
+        if domain is not None and result is True:
+            result = parse_certificate(domain)
+        if result != "fail":
+            send = {
+                "status": "OK",
+                "certificates": result,
+            }
+        return jsonify(send)
+    except (JSONDecodeError, URLValidationError):
+        send = {
+            "status": "OK",
+            "message": "Please ensure that the url is correct",
+        }
+        return jsonify(send)
 
 
 def parse_certificate(domain: str) -> List[Any]:
@@ -44,6 +54,23 @@ def parse_certificate(domain: str) -> List[Any]:
         data = certigo_result
         return data
     except FileNotFoundError:
-        return [{
-            "message": "Something went wrong and we're investigating the cause"
-        }]
+        return [{"message": "Something went wrong and we're investigating the cause"}]
+
+
+def sanitize_input(url_string: str) -> bool:
+    """
+    Checks if the user input to the request parameter does not pwn the application
+    """
+    regex = "^((?!-)[A-Za-z0-9-]" + "{1,63}(?<!-)\\.)" + "+[A-Za-z]{2,6}"
+    pattern = re.compile(regex)
+    if pattern.match(url_string):
+        return True
+    raise URLValidationError()
+
+
+class URLValidationError(Exception):
+    """
+    URLValidationError error is raised when the supplied url does not match a domain pattern
+    """
+    def __init__(self):
+        pass
